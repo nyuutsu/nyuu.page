f666603982f646766c3c7f255232016a141a081e
nyuu posts <nyuutsu@protonmail.com>
2023-02-03 16:28:17 -0500

    pelicanconf most obvious changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
 
-AUTHOR = 'Eevee'
-SITENAME = 'fuzzy notepad'
+AUTHOR = 'nyuu'
+SITENAME = 'nyuu dot page'
 SITEURL = ''
 #SITESUBTITLE = ...
 
-TIMEZONE = 'America/Los_Angeles'
+TIMEZONE = 'America/New_York'
 DEFAULT_DATE_FORMAT = '%a %b %d, %Y'
 
 DEFAULT_LANG = u'en'
@@ -23,85 +21,50 @@ AUTHOR_FEED_RSS = None
 # NOTE: These aren't called just LINKS and SOCIAL because those are assumed by
 # the default theme to be 2-tuples, but I need more info.
 LINKS_EX = ((
-    'blog',
-    '/blog/',
-    '#f6b441', 'category-blog.png',
-    "detailed, thoughtful prose about why computers are the worst",
+  'blog',
+  '/blog/',
+  '#f6b441', 'category-blog.png',
+  "detailed, thoughtful prose about why computers are the worst",
 ), (
-    'release',
-    '/release/',
-    '#4a83c5', 'category-release.png',
-    "things i've released into the wild, and maybe my thoughts on them",
+  'release',
+  '/release/',
+  '#4a83c5', 'category-release.png',
+  "things i've released into the wild, and maybe my thoughts on them",
 ), (
-    'art',
-    '/art/',
-    '#41c518', 'category-art.png',
-    "i'm learning to draw, here are the results",
+  'art',
+  '/art/',
+  '#41c518', 'category-art.png',
+  "i'm learning to draw, here are the results",
 ), (
-    'everything',
-    '/everything/',
-    '#c57be6', 'category-everything.png',
-    "why limit yourself when you can have it all",
+  'everything',
+  '/everything/',
+  '#c57be6', 'category-everything.png',
+  "why limit yourself when you can have it all",
 ), (
-    'archives',
-    '/everything/archives/',
-    '#399ccd', 'category-archives.png',
-    "turn the clock back to æons past",
+  'archives',
+  '/everything/archives/',
+  '#399ccd', 'category-archives.png',
+  "turn the clock back to æons past",
 ))
 # FIXME this is duplicated from home.html; maybe consolidate
 SOCIAL_EX = ((
-    'email',
-    'mailto:eevee.fuzzynotepad@veekun.com',
-    '#9966cc', 'logo-email.png',
-    "do people still use this",
+  'email',
+  'mailto:nyuutsu@protonmail.com',
+  '#9966cc', 'logo-email.png',
+  "do people still use this",
 ), (
-    'twitter',
-    'https://twitter.com/eevee',
-    '#55acee', 'logo-twitter.png',
-    "firehose of bad jokes",
+  'twitter',
+  'https://twitter.com/shroomwaview',
+  '#55acee', 'logo-twitter.png',
+  "firehose of bad jokes",
 ), (
-    'mastodon',
-    'https://mastodon.social/@eevee',
-    '#3088d4', 'logo-mastodon.png',
-    "trickle of bad jokes",
-), (
-    'square',
-    'https://cash.me/$eevee',
-    '#29c501', 'logo-square-cash.png',
-    "just give me money?",
-), (
-    'paypal',
-    'https://www.paypal.me/lexyeevee',
-    '#009cde', 'logo-paypal.png',
-    "just give me money, again?",
-), (
-    'github',
-    'https://github.com/eevee',
-    '#4183c4', 'logo-github.png',
-    "lots of abandoned code",
-), (
-    'art tumblr',
-    'https://lexyeevee.tumblr.com/',
-    '#35465c', 'logo-tumblr.png',
-    "just art",
-), (
-    'itch.io',
-    'https://eevee.itch.io/',
-    '#fa5c5c', 'logo-itch.png',
-    "indie games",
-), (
-    'twitch',
-    'https://twitch.tv/lexyeevee',
-    '#6441a4', 'logo-twitch.png',
-    "occasional old game streams",
-), (
-    'youtube',
-    'https://www.youtube.com/user/lexyeevee',
-    '#cc181e', 'logo-youtube.png',
-    "mostly cat videos",
+  'github',
+  'https://github.com/nyuutsu',
+  '#4183c4', 'logo-github.png',
+  "lots of abandoned code",
 ))
-TWITTER_USERNAME = 'eevee'
-GITHUB_URL = 'https://github.com/eevee'
+TWITTER_USERNAME = 'shroomwaview'
+GITHUB_URL = 'https://github.com/nyuutsu'
 # i hate this stupid eyesore and i'm pretty sure my audience knows how to
 # search a website with their favorite search engine
 SEARCH_BOX = False
@@ -109,37 +72,37 @@ SEARCH_BOX = False
 DEFAULT_PAGINATION = 17
 DEFAULT_ORPHANS = 4
 PAGINATION_PATTERNS = (
-    (1, '{base_name}/', '{base_name}/index.html'),
-    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+  (1, '{base_name}/', '{base_name}/index.html'),
+  (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
 )
 
 THEME = 'theme'
 
 def sort_by_article_count(tags):
-    return sorted(tags, key=lambda pairs: len(pairs[1]), reverse=True)
+  return sorted(tags, key=lambda pairs: len(pairs[1]), reverse=True)
 
 JINJA_FILTERS = dict(
-    sort_by_article_count=sort_by_article_count,
+  sort_by_article_count=sort_by_article_count,
 )
 
 EXTRA_HEADER = """
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.colorbox/1.6.4/jquery.colorbox-min.js"></script>
 <script type="text/javascript">
-    $(function() {
-        $('article').each(function(index, article) {
-            $(article).find('a.photo').colorbox({
-                fixed: true,
-                maxWidth: '100%',
-                maxHeight: '100%',
-                rel: 'colorbox' + String(index + 1),
-                // Text
-                previous: '←',
-                next: '→',
-                close: '×',
-            });
-        });
+  $(function() {
+    $('article').each(function(index, article) {
+      $(article).find('a.photo').colorbox({
+        fixed: true,
+        maxWidth: '100%',
+        maxHeight: '100%',
+        rel: 'colorbox' + String(index + 1),
+        // Text
+        previous: '←',
+        next: '→',
+        close: '×',
+      });
     });
+  });
 </script>
 """
 
@@ -147,22 +110,22 @@ EXTRA_HEADER = """
 TYPOGRIFY = True
 # Stop putting &nbsp; in the fucking article titles
 TYPOGRIFY_IGNORE_TAGS = [
-    'pre', 'code', 'header', 'h1', 'h2', 'h3', 'aside',
+  'pre', 'code', 'header', 'h1', 'h2', 'h3', 'aside',
 ]
 
 MARKDOWN = dict(
-    extension_configs={
-        'markdown.extensions.codehilite': dict(
-            css_class='highlight',
-            linenums=True,
-        ),
-        'markdown.extensions.extra': {},
-        # GitHub-style fenced code blocks
-        'markdown.extensions.fenced_code': {},
-        # I don't actually care about a table of contents, but this turns headers
-        # into self-links
-        'markdown.extensions.toc': dict(anchorlink=True),
-    },
+  extension_configs={
+    'markdown.extensions.codehilite': dict(
+      css_class='highlight',
+      linenums=True,
+    ),
+    'markdown.extensions.extra': {},
+    # GitHub-style fenced code blocks
+    'markdown.extensions.fenced_code': {},
+    # I don't actually care about a table of contents, but this turns headers
+    # into self-links
+    'markdown.extensions.toc': dict(anchorlink=True),
+  },
 )
 
 PATH = 'content/'
@@ -175,7 +138,7 @@ READERS = dict(html=None)
 
 # For the landing page
 TEMPLATE_PAGES = {
-    '../theme/templates/home.html': 'index.html',
+  '../theme/templates/home.html': 'index.html',
 }
 
 # URL schema; compatible with Octopress, but i happen to like it anyway
@@ -220,22 +183,22 @@ import eeveeblog.liquid_photo
 from eeveeblog.rgbasm_lexer import RGBASMLexer
 import pygments.lexers._mapping
 pygments.lexers._mapping.LEXERS['RGBASMLexer'] = (
-    'eeveeblog.rgbasm_lexer',
-    RGBASMLexer.name,
-    tuple(RGBASMLexer.aliases),
-    tuple(RGBASMLexer.filenames),
-    tuple(RGBASMLexer.mimetypes))
+  'eeveeblog.rgbasm_lexer',
+  RGBASMLexer.name,
+  tuple(RGBASMLexer.aliases),
+  tuple(RGBASMLexer.filenames),
+  tuple(RGBASMLexer.mimetypes))
 
 PLUGIN_PATHS = ["pelican-plugins.git"]
 PLUGINS = [
-    eeveeblog.liquid_gallery,
-    eeveeblog.liquid_illus,
-    eeveeblog.liquid_photo,
-    'summary',
-    'custom_article_urls',
-    'photos',
-    'render_math',
-    'thumbnailer',
+  eeveeblog.liquid_gallery,
+  eeveeblog.liquid_illus,
+  eeveeblog.liquid_photo,
+  'summary',
+  'custom_article_urls',
+  'photos',
+  'render_math',
+  'thumbnailer',
 ]
 
 # Plugin config for summary
@@ -248,10 +211,10 @@ SUMMARY_MAX_LENGTH = None
 # Plugin config for custom article urls
 # Preserve the old blog URL for blog stuff
 CUSTOM_ARTICLE_URLS = {
-    'blog': dict(
-        URL='{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}/',
-        SAVE_AS='{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html',
-    ),
+  'blog': dict(
+    URL='{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}/',
+    SAVE_AS='{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html',
+  ),
 }
 
 # Plugin config for photos
